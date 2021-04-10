2122fc0398e78143fd9df62b86cc842c5d2fe157
Conrado A. Cardozo <cacardoso@tjba.jus.br>
2021-04-10 16:34:37 -0300

    Aula 3

--- js/main.js
+++ js/main.js
@@ -8,7 +8,14 @@ var lista = ["Beto","Ricardo","Luiz"];
 lista.push("Amanda");
 console.log(lista);
 
+var livros = [{nome:"Beto ", cor:"preta"},{nome:"Vera ", cor:"branca"}];
+console.log(livros);
+console.log(livros[0].nome);
 
 var livro = {nome:"Beto ", cor:"preta"};
-
 console.log(livro.nome + livro.cor);
+
+function clicou() {
+    document.getElementById("agradecimento").innerHTML = "Obragado por clicar";
+}
+
