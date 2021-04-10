dfdb71160d502bee56f08172c2c720bd257c8d95
Conrado A. Cardozo <cacardoso@tjba.jus.br>
2021-04-10 14:47:41 -0300

    Aula 2

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 var nome = 'conrado'
 alert("meu primeiro javascript " + nome);
+
+var lista = ["Beto","Ricardo","Luiz"];
+console.log(lista);
+
+var lista = ["Beto","Ricardo","Luiz"];
+lista.push("Amanda");
+console.log(lista);
+
+
+var livro = {nome:"Beto ", cor:"preta"};
+
+console.log(livro.nome + livro.cor);
